34f5a445d7cbefb1ea9daa86c8ee9a0661fa7d11
Wrubleski <henrike.hip@live.com>
2021-01-20 19:11:43 -0300

    Commit inicial. Criado o esqueleto do projeto.

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.post("/api/loot", (req, res) => {
+  res.send("Tibia Loot API test." + req.body);
+});
+
+app.listen(process.env.PORT || 3001, (req, res) => {
+  console.log("Server is up and running.");
+});
