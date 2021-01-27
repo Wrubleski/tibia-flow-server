c6aa669d83d09be0362718465ca4d9a046aeadd8
Wrubleski <henrike.hip@live.com>
2021-01-26 22:51:08 -0300

    Calculo de loot funcionando. Aberto para refatoramento.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const LootCalculatorService = require("./src/services/LootCalculatorService");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
